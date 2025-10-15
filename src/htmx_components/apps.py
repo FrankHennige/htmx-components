11e9cc983824112d80f1e0b868d2758c80c209e8
Frank Hennige <frank@hennige-it.de>
2025-10-15 17:49:28 +0200

    ci: Test

--- src/htmx_components/apps.py
+++ src/htmx_components/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class HtmxComponentsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'htmx_components'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "htmx_components"
